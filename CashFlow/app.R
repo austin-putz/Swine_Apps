fd9b5e0f89f233550bcfa21a3a11c3f21d995fd3
austin-putz <putz.austin@gmail.com>
2017-01-16 17:52:33 -0600

    updated format of app.R

--- CashFlow/app.R
+++ CashFlow/app.R
@@ -12,147 +12,241 @@
 #
 
 library(shiny)
+library(shinydashboard)
 library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  tags$h1("Contract Cash Flow"),
-  tags$img(height=200, width=200, 
-    src="istate_logo.jpg"),
-  tags$p("This Shiny App is used to calculate the profit for a contract
-    producer."),
+  theme = "bootstrap-theme.css",
+  
+  #----------------------------------------#
+  # Header
+  #----------------------------------------#
+  
+  # Add Title to program
+  # tags$h1("Contract Cash Flow"),
+  HTML('
+    <h1>
+    Contract Cash Flow
+    </h1>
+    '),
+  
+  # Add IA State Logo to program
+  # tags$img(height=200, width=200, src="istate_logo.jpg"),
+  HTML('
+    <img src="istate_logo.jpg" alt="IA State Logo" style="width:200px;height:200px;"> 
+    '), 
+  
+  # Write a short description
+  # tags$p("This Shiny App is used to calculate the profit for a contract
+  #   producer."),
+  HTML('
+  <ul>
+  <li> This is a Shiny App to calculate returns, costs, and profit for
+  a contract producer. </li>
+  </ol>
+  '),
+  
+  # Line break
   tags$hr(),
-   
-   # Application title
-   # titlePanel("Cash Flow (Contract)"),
-   
-   # Sidebar with a slider input for number of bins 
-   #sidebarLayout(
-      #sidebarPanel(
-         sliderInput(inputId="years",
+  
+  #----------------------------------------#
+  # Begin Tabs
+  #----------------------------------------#
+  
+  # Start panel subset
+  #tabsetPanel(
+    #tabPanel("Inputs",
+  
+  tags$h2("Inputs"),
+  
+    tags$h3("General Info"),
+  
+       wellPanel(
+        fluidRow(
+         column(4, sliderInput(inputId="years",
                      label="Number of years (useful life):",
                      min = 5,
                      max = 35,
-                     value = 24, step=1),
-         sliderInput(inputId="capacity",
+                     value = 24, step=1)),
+         column(4, sliderInput(inputId="capacity",
                      label="Capacity of barn:",
                      min = 100,
                      max = 20000,
-                     value = 2400, step=100),
+                     value = 2400, step=100)),
          # sliderInput(inputId="turns",
          #             label="Turns per year:",
          #             min = 1,
          #             max = 3,
          #             value = 1, step=1),
-         sliderInput(inputId="payment",
+         column(4, sliderInput(inputId="payment",
                      label="Payment per head:",
                      min = 30,
                      max = 70,
-                     value = 43, step=1),
-         sliderInput(inputId="wageRate",
-                     label="Wage Rate per Hour:",
-                     min = 0,
-                     max = 30.00,
-                     value = 0, step=0.50),
-         sliderInput(inputId="laborHours",
-                     label="Labor per pig (hours):",
-                     min = 0,
-                     max = 1,
-                     value = 0.40, step=0.1),
-         sliderInput(inputId="contractLength",
+                     value = 43, step=1))),
+  
+        fluidRow(
+         column(4, sliderInput(inputId="contractLength",
                      label="Length of contract:",
                      min = 1,
                      max = 20,
-                     value = 15, step=1),
-         # sliderInput(inputId="usefulLife",
-         #             label="Useful life of building:",
-         #             min = 10,
-         #             max = 30,
-         #             value = 24, step=1),
-         sliderInput(inputId="taxRate",
+                     value = 15, step=1)),
+         column(4, sliderInput(inputId="taxRate",
                      label="Tax Rate (%):",
                      min = 0,
                      max = 50,
-                     value = 15, step=1),
-         numericInput(inputId="landCost",
+                     value = 15, step=1)))
+       ),
+  
+      # Line Break
+      tags$hr(),
+  
+    tags$h3("Building costs"),
+  
+       wellPanel(
+        fluidRow(
+         column(4, numericInput(inputId="landCost",
                      label="Land Cost:",
                      min = 0,
                      max = 100000,
-                     value = 0, step=500),
-         numericInput(inputId="sitePrep",
+                     value = 0, step=500)),
+         column(4, numericInput(inputId="sitePrep",
                      label="Site Preparation Cost:",
                      min = 0,
                      max = 100000,
-                     value = 60000, step=500),
-         numericInput(inputId="buildingCost",
+                     value = 60000, step=500))),
+        fluidRow(
+         column(4, numericInput(inputId="buildingCost",
                      label="Building Cost:",
                      min = 0,
                      max = 1000000,
-                     value = 316000, step=1000),
-         numericInput(inputId="equipmentCost",
+                     value = 316000, step=1000)),
+         column(4, numericInput(inputId="equipmentCost",
                      label="Equipment Cost:",
                      min = 0,
                      max = 1000000,
-                     value = 258000, step=1000),
-         numericInput(inputId="equityContribution",
+                     value = 258000, step=1000)))
+       ),
+  
+      # Line Break
+      tags$hr(),
+  
+    tags$h3("Loan Terms"),
+  
+       wellPanel(
+        fluidRow(
+         column(4, numericInput(inputId="equityContribution",
                      label="Equity Contribution:",
                      min = 0,
                      max = 100000,
-                     value = 0, step=1000),
-         sliderInput(inputId="loanLength",
+                     value = 0, step=1000)),
+         column(4, sliderInput(inputId="loanLength",
                      label="Loan Length (years):",
                      min = 1,
                      max = 20,
-                     value = 12, step=1),
-         sliderInput(inputId="loanTaxRate",
+                     value = 12, step=1)),
+         column(4, sliderInput(inputId="loanTaxRate",
                      label="Loan Tax Rate (%):",
                      min = 0,
                      max = 30,
-                     value = 4, step=1),
-         sliderInput(inputId="maintenance",
+                     value = 4, step=1)))
+       ),
+  
+      # Line Break
+      tags$hr(),
+  
+    tags$h3("Variable costs"),
+  
+       wellPanel(
+        fluidRow(
+         column(4, sliderInput(inputId="maintenance",
                      label="Maintenance costs:",
                      min = 0,
                      max = 10000,
-                     value = 2400, step=200),
-         sliderInput(inputId="utilities",
+                     value = 2400, step=200)),
+         column(4, sliderInput(inputId="utilities",
                      label="Utility costs:",
                      min = 0,
                      max = 30000,
-                     value = 16000, step=500),
-         sliderInput(inputId="insurance",
+                     value = 16000, step=500)),
+         column(4, sliderInput(inputId="insurance",
                      label="Insurance costs:",
                      min = 0,
                      max = 10000,
-                     value = 3200, step=200),
-         sliderInput(inputId="taxes",
+                     value = 3200, step=200))),
+  
+        fluidRow(
+         column(4, sliderInput(inputId="taxes",
                      label="Tax costs:",
                      min = 0,
                      max = 10000,
-                     value = 2100, step=100),
-         sliderInput(inputId="waste",
+                     value = 2100, step=100)),
+         column(4, sliderInput(inputId="waste",
                      label="Waste Management costs:",
                      min = 0,
                      max = 20000,
-                     value = 11000, step=500),
-         sliderInput(inputId="fertilizer",
+                     value = 11000, step=500)),
+         column(4, sliderInput(inputId="fertilizer",
                      label="Fertilizer value per year:",
                      min = 0,
                      max = 50000,
-                     value = 25000, step=500),
-        # actionButton(inputId="rerun", 
-        #              label="Rerun Cash Flow"),
-      #),
-      
-      # Show a plot of the generated distribution
-      #mainPanel(
-      plotOutput("profitPlot"),
-      #)
-     
-     verbatimTextOutput("summary")
+                     value = 25000, step=500))),
+        fluidRow(
+         column(4, sliderInput(inputId="wageRate",
+                     label="Wage Rate per Hour:",
+                     min = 0,
+                     max = 30.00,
+                     value = 0, step=0.50)),
+         column(4, sliderInput(inputId="laborHours",
+                     label="Labor per pig (hours):",
+                     min = 0,
+                     max = 1,
+                     value = 0.40, step=0.1)))
+         
+       ),
+    #),
+  
+  # tabPanel("Outputs",
+  
+    # Line Break
+    tags$hr(),
+  
+  tags$h2("Output"),
+  
+    # header for Profit Plot
+    tags$h3("Profit Plot"),
+  
+      # Show a plot of the profit
+      fluidRow(column(8, offset=2, 
+        plotOutput("profitPlot"))),
+  
+    # Line Break
+    tags$hr(),
+  
+    # header for dataset output
+    tags$h3("Dataset from Inputs"),
+  
+      # Print the Data Frame calculated from inputs
+      dataTableOutput("dataoutput")
+  
+    #) # END tabPanel()
+  #) # END tabsetPanel()
+) # END fluidPage()
 
-   #)
-)
+
+
+
+
+
+
+
+
+
+
+#==============================================================================#
+# Server
+#==============================================================================#
 
 # Plot profit for cash flow example
 server <- function(input, output) {
@@ -236,6 +330,7 @@ server <- function(input, output) {
       
    })
    
+   # Add the Profit Plot
    output$profitPlot <- renderPlot({
 
       # plot Profit by year
@@ -248,12 +343,37 @@ server <- function(input, output) {
         theme(text=element_text(size=18))
       
    })
-   output$summary <- renderPrint({
-     summary(data()$Profit)
+
+   # Add Data table with all the values calculated above 
+   output$dataoutput <- renderDataTable({
+     data()
    })
 }
 
 
+
+
+
+
+
+
+
+
+
+#==============================================================================#
+# Shiny App
+#==============================================================================#
+
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
 
